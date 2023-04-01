f480371f9ef5a8d940c5530e9b76994a89ecf1a8
Szymon Zygmunt <szymonzygmuntzsem@gmail.com>
2023-04-01 22:22:44 +0200

    first minor styling

--- Content.js
+++ Content.js
@@ -1,11 +1,34 @@
+let bgColor = '#191A30'
+let contentColor = '#141425'
+let secondaryColor = '#3E4074'
+let textColor = '#b1b1b1'
+console.log(1)
 
-let tablicaZawartosc = $('.tablicaZawartosc');
-let nav = $('.nav-wrapper');
-let card = $('.card');
-let btn = $('.btn-flat, .btn');
-let links = $('a');
-let cardTitle = $('.card-title');
-let widget = $('.widgetClouda');
+console.log(2)
+let layoutBody = document.querySelector('#layoutBody')
+let navWrapper = document.querySelector('.nav-wrapper')
+let materialIcons = document.querySelectorAll('.material-icons')
+let cards = document.querySelectorAll('.tablicaZawartosc')
+
+//STYLING
+layoutBody.style.backgroundColor = bgColor;
+navWrapper.style.backgroundColor = bgColor;
+
+materialIcons.forEach((icon) => {
+    icon.style.color = 'grey'
+})
+
+cards.forEach((card) => {
+    card.style.background = contentColor;
+    card.style.color = textColor;
+})
+
+
+
+
+
+
+
 
 
 
